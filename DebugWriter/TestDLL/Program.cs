57306e59ef84db7c71e84c2b1d8caab96bea8ac5
yt-kobayashi <270yoshikoba@gmail.com>
2020-11-15 14:17:47 +0900

    [ADD] テストプロジェクト作成

---
+++ DebugWriter/TestDLL/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DebugWriterLib;
+
+namespace TestDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Debug debug = new Debug("Sample.xml", 4);
+            debug.SampleXmlFile("Sample.xml");
+            DebugWriter writer = new DebugWriter(Mode.Debug, "Sample.xml");
+
+            for (int count = 0; count < 2000; count++)
+            {
+                writer.Writer.OutputMessage(count, "TEST");
+            }
+        }
+    }
+}
