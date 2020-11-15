c9ef241085533a79c8638b4dd888d2c69e2b0d61
yt-kobayashi <270yoshikoba@gmail.com>
2020-11-15 11:50:25 +0900

    [MOD] Xmlファイル読み込みロジック実装

--- DebugWriter/DebugWriter/DebugWriter.cs
+++ DebugWriter/DebugWriter/DebugWriter.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using XMLStreamWrapper;
 
 namespace DebugWriter
 {
@@ -17,39 +20,97 @@ namespace DebugWriter
 
     public class Base
     {
-        public Base()
+        protected struct DebugMessage
         {
-
+            public Mode Mode;
+            public int Number;
+            public string Message;
         }
 
-        public Base(in string filePath)
-        {
+        private Dictionary<int, string> DebugMessages;
+        private Dictionary<int, string> ErrorMessages;
+        private Dictionary<int, string> StatusMessages;
+        private Dictionary<int, string> ReleaseMessages;
 
+        /// <summary>
+        /// 空コンストラクタ
+        /// </summary>
+        public Base() { }
+
+        protected void InitializeDictionary()
+        {
+            DebugMessages = new Dictionary<int, string>();
+            ErrorMessages = new Dictionary<int, string>();
+            StatusMessages = new Dictionary<int, string>();
+            ReleaseMessages = new Dictionary<int, string>();
+        }
+
+        protected void ReadMessageXml(in string filePath)
+        {
+            // Dictionaryの初期化
+            InitializeDictionary();
+
+            // ファイルが存在しない場合
+            if (false == File.Exists(filePath))
+            {
+                return;
+            }
+
+            // Xmlファイルを読み込む
+            List<DebugMessage> messages = new List<DebugMessage>();
+            SimpleXmlSerializerWrapper<List<DebugMessage>> serializer = new SimpleXmlSerializerWrapper<List<DebugMessage>>(messages, filePath);
+            messages = serializer.Deserialize();
+            // Dictionaryにデータを詰める
+            SetMessages(messages);
+        }
+
+        protected void SetMessages(in List<DebugMessage> messages)
+        {
+            foreach(DebugMessage message in messages)
+            {
+                switch (message.Mode)
+                {
+                    case Mode.Debug:
+                        DebugMessages.Add(message.Number, message.Message);
+                        break;
+                    case Mode.Error:
+                        ErrorMessages.Add(message.Number, message.Message);
+                        break;
+                    case Mode.Status:
+                        StatusMessages.Add(message.Number, message.Message);
+                        break;
+                    case Mode.Release:
+                        ReleaseMessages.Add(message.Number, message.Message);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 
-    public class DebugWriter : Writer
+    public class DebugWriter : Base, Writer
     {
         public void OutputMessage()
         {
         }
     }
 
-    public class ErrorWriter : Writer
+    public class ErrorWriter : Base, Writer
     {
         public void OutputMessage()
         {
         }
     }
 
-    public class StatusWriter : Writer
+    public class StatusWriter : Base, Writer
     {
         public void OutputMessage()
         {
         }
     }
 
-    public class ReleaseWriter : Writer
+    public class ReleaseWriter : Base, Writer
     {
         public void OutputMessage()
         {
