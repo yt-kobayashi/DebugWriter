b70e92479443341007d484570a4b36f4db0aae17
yt-kobayashi <270yoshikoba@gmail.com>
2020-11-17 02:11:23 +0900

    [MOD] ExcelファイルからXmlファイルへの変換ロジックを実装

--- DebugWriter/DebugWriter/Debug.cs
+++ DebugWriter/DebugWriter/Debug.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using ClosedXML.Excel;
 using XMLStreamWrapper;
 
-namespace DebugWriterLib
+namespace DebuggerLib
 {
     public enum Mode
     {
@@ -54,6 +55,120 @@ namespace DebugWriterLib
                     default:
                         break;
             }
+        }
+
+        public void XlsxToXml(in string xlsxFilePath, in string xmlFilePath, in int startRowNum = 2)
+        {
+            List<DebugMessage> messages = new List<DebugMessage>();                     // デバッグメッセージリスト
+            string extension = xlsxFilePath.Substring(xlsxFilePath.Length - 5);         // 拡張子
+            SimpleXmlSerializerWrapper<List<DebugMessage>> serialize = new SimpleXmlSerializerWrapper<List<DebugMessage>>(messages, xmlFilePath);
+
+            // ファイルが存在しない場合は変換終了
+            if(false == File.Exists(xlsxFilePath))
+            {
+                Console.WriteLine("Xlsx file not found!");
+                return;
+            }
+
+            // 拡張子がxlsxかXlsxのもの出なければ変換終了
+            if(".xlsx" != extension && ".Xlsx" != extension)
+            {
+                Console.WriteLine("Not xlsx or Xlsx file!");
+                return;
+            }
+
+            // ExcelファイルからDebugMessageListを生成できなければ変換終了
+            if(false == SetDebugMessageList(xlsxFilePath, startRowNum, ref messages))
+            {
+                Console.WriteLine("Failed set DebugMessage list!");
+                return;
+            }
+
+            serialize.Serialize();
+        }
+
+        private bool SetDebugMessageList(in string xlsxFilePath, in int startRowNum, ref List<DebugMessage> messages)
+        {
+            // ワークブック取得
+            XLWorkbook workbook = new XLWorkbook(xlsxFilePath);
+            foreach(IXLWorksheet worksheet in workbook.Worksheets)
+            {
+                // 最終行番号を取得
+                int lastRowNum = worksheet.LastRowUsed().RowNumber();
+
+                // 取得を開始する行が範囲外なら変換終了
+                if(startRowNum < 1 || lastRowNum <= startRowNum)
+                {
+                    Console.WriteLine("Not in the range of 1 to last row number!");
+                    return false;
+                }
+
+                // 各要素を取得
+                // 1行目は見出しと考えて処理をしない
+                for (int rowNum = startRowNum; rowNum <= lastRowNum; rowNum++)
+                {
+                    DebugMessage debugMessage = new DebugMessage();         // デバッグメッセージ
+
+                    // パラメータが入っているセルを取得
+                    IXLCell mode = worksheet.Cell(rowNum, 1);
+                    IXLCell num = worksheet.Cell(rowNum, 2);
+                    IXLCell message = worksheet.Cell(rowNum, 3);
+
+                    // デバッグメッセージを取得できなかった場合は次の処理に移る
+                    if (false == SetDebugMessage(mode.Value.ToString(), num.Value.ToString(), message.Value.ToString(), ref debugMessage))
+                    {
+                        Console.WriteLine("Failed set DebugMessage!");
+                        continue;
+                    }
+
+                    // デバッグメッセージをリストに追加する
+                    messages.Add(debugMessage);
+                }
+            }
+
+            return true;
+        }
+
+        private bool SetDebugMessage(in string mode, in string num, in string message, ref DebugMessage result)
+        {
+            Mode tempMode = Mode.Debug;
+            int tempNumber = 0;
+            string tempMessage = "DebugMessage";
+
+            // モード選択
+            switch (mode)
+            {
+                case "Debug":
+                    tempMode = Mode.Debug;
+                    break;
+                case "Error":
+                    tempMode = Mode.Error;
+                    break;
+                case "Status":
+                    tempMode = Mode.Status;
+                    break;
+                case "Release":
+                    tempMode = Mode.Release;
+                    break;
+                default:
+                    Console.WriteLine("Mode not found! : " + mode);
+                    return false;
+                    break;
+            }
+
+            // メッセージ番号をstringからintへ変換
+            if(false == int.TryParse(num, out tempNumber))
+            {
+                Console.WriteLine("Not number!");
+                return false;
+            }
+
+            // パラメータ設定
+            result.Mode = tempMode;
+            result.Number = tempNumber;
+            result.Message = tempMessage;
+
+            return true;
         }
     }
 
